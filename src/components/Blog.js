641e422001a6df60fc6c894f9e6d4237f4ed75a3
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-10-11 16:15:20 +0600

    blog setup done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,7 +5,9 @@ function Blog() {
   return (
     <div>
         <Navigation></Navigation>
-        <h1>Blog</h1>
+        <h1>1. What is the purpose of react router?</h1>
+        <h1>2. How does context api work?</h1>
+        <h1>3. What is the purpose of useRef Hook</h1>
     </div>
   )
 }
