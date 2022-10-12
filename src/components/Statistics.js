917c7eba8382462173907930c5d0134bd580f8f8
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-10-12 06:03:38 +0600

    Chart done

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Navigation from './Navigation'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis,ResponsiveContainer } from 'recharts';
+import './statistics.css';
 
 function Statistics() {
+    const [skills, setSkills] = useState([]);
+
+    useEffect( () => {
+        fetch('https://openapi.programming-hero.com/api/quiz')
+        .then(res=> res.json())
+        .then(data=> setSkills(data.data));
+    }, [])
+
+    let data = skills;
+
+    // skills.map(skill => {
+    //     let d = {
+    //         name: skill.name,
+    //         uv: skill.total
+    //     }
+    //     data.push(d);
+    // })
+
   return (
     <div>
         <Navigation></Navigation>
-      <h1>Statistics</h1>
+
+        <ResponsiveContainer width="100%" height={600}>
+            <LineChart  data={data}>
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey="name" />
+                <YAxis />
+            </LineChart>
+        </ResponsiveContainer>
     </div>
   )
 }
